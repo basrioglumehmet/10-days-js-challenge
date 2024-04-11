b2d1778648dd40429048d06a6a599c85237df696
basrioglumehmet <166669195+basrioglumehmet@users.noreply.github.com>
2024-04-11 06:08:19 +0300

    Create kok_hesaplamasi_literal.js

---
+++ day5/kok_hesaplamasi_literal.js
@@ -0,0 +1,61 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    //Tanimlamalar
+    const [area,perimeter] = expressions;
+    //Kok sayi hesaplamasi
+    const discriminant = Math.sqrt(perimeter * perimeter - 16 * area)
+    //S1 iki kenar toplami
+    const s1 = (perimeter + discriminant) / 4;
+    // S2 iki kenar uzunlugu
+    const s2 = (perimeter - discriminant) / 4
+    
+    return [s1,s2].sort((x,y) => x - y )
+
+    
+}
+
+
+
+function main() {
+    let s1 = +(readLine());
+    let s2 = +(readLine());
+    
+    [s1, s2] = [s1, s2].sort();
+    
+    const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
+    
+    console.log((s1 === x) ? s1 : -1);
+    console.log((s2 === y) ? s2 : -1);
+}
